83d8580a5f70c094d256259f35fb69eb0d8d3e3e
Shivam Tyagi <99984464+shivamtyagi2000@users.noreply.github.com>
2022-02-19 20:53:55 +0530

    Time: 8 ms (26.72%), Space: 5.9 MB (74.65%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,11 +1,26 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long long y=0;
+     /*   long long y=0;
         while(y*y<=x)
         {
             y++;
         }
         return y-1;
+        */
+        //better apporach
+            long long s=0,e=INT_MAX,ans;
+        while(s<=e)
+        {
+            long long mid=(s+e)/2;
+            if(mid*mid<=x)
+            {
+                ans=mid;
+                s=mid+1;
+            }
+            else
+                e=mid-1;
+        }
+        return ans;
     }
 };
