329ec05d14bce7bc658f22e8ca5947182c4659eb
Shivam Tyagi <99984464+shivamtyagi2000@users.noreply.github.com>
2022-02-22 13:45:41 +0530

    Time: 5 ms (90.76%), Space: 7.3 MB (78.28%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int a[26]={0},b[26]={0};
+        for(int i=0;i<s.size();i++)
+            a[s[i]-'a']++;
+         for(int i=0;i<t.size();i++)
+            b[t[i]-'a']++;
+        for(int i=0;i<26;i++)
+        {
+            if(a[i]==b[i])
+                continue;
+            else
+                return false;
+        }
+        return true;
+    }
+};
