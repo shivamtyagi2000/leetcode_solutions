ef925a30f070709c0ce5f6595a6970f12e86fb51
Shivam Tyagi <99984464+shivamtyagi2000@users.noreply.github.com>
2022-04-14 10:32:37 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (75.74%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     string freqAlphabets(string s) {
-        string ans="";
-        for(int i=0;i+2<s.size();i++){
-            if(s[i+2]=='#'){
-                ans+='a'+10*(s[i]-'0')+s[i+1]-'0'-1;
-                i+=2;
+		int n = s.length(), i = 0;
+        string ans = "";
+        while(i < n){
+            if(i + 2 < n && s[i + 2] == '#') {
+                int ss = (s[i]-'0')*10 + (s[i+1]-'0');
+                ans += char(ss+'a'-1);
+                i += 3;
             }
-            else{
-                ans+='a'+s[i]-'0'-1;
+            else {
+                ans += char(s[i]-'0'-1+'a');
+                i++;
             }
-        }
-        if(s[s.size()-1]!='#'&&s[s.size()-2]!='#'){
-           { ans+='a'+s[s.size()-2]-'0'-1;
-             ans+=+'a'+s[s.size()-1]-'0'-1;
-            }
-        }
-        if(s[s.size()-2]=='#'){
-            ans+='a'+s[s.size()-1]-1-'0';
-        }
+    }
         return ans;
     }
 };
