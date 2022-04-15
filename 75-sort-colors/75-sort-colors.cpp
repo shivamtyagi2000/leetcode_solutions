47a2b40c9be7f2c00a1dad63ab1e79cd4342cdd8
Shivam Tyagi <99984464+shivamtyagi2000@users.noreply.github.com>
2022-04-15 20:33:11 +0530

    Time: 3 ms (58.34%), Space: 8.3 MB (24.47%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        
+        /*approch 1
+        sort(nums.begin(),nums.end());
+        */
+        
+       /* approch 2
         int count_red=0,count_blue=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]==0)
@@ -22,6 +28,22 @@ public:
                   }
                 else
                     nums[i]=2;
+            }
+        }*/
+        
+        //best approch
+        int low=0,mid=0,end=nums.size()-1;
+        while(mid<=end){
+            switch(nums[mid]){
+                case 0 :
+                    swap(nums[mid++],nums[low++]);
+                    break;
+                case 1 :
+                    mid++;
+                    break;
+                case 2:
+                    swap(nums[mid],nums[end--]);
+                    break;
             }
         }
     }
