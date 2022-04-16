ea11abdf9382643060424ac0d0e22415782c80b9
Shivam Tyagi <99984464+shivamtyagi2000@users.noreply.github.com>
2022-04-17 03:41:40 +0530

    Time: 3 ms (61.95%), Space: 7 MB (92.62%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n=matrix.size();
+       
+       int n=matrix.size(),temp;
+        /*brute force
         vector<vector<int>> ans(n,vector<int> (n,0));
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
@@ -11,5 +13,27 @@ public:
         for(int i=0;i<n;i++)
             for(int j=0;j<n;j++)
                 matrix[i][j]=ans[i][j];
+                */
+        //best approach
+        // transpose+ reverse row
+        for(int i=0;i<n;i++)
+            for(int j=0;j<i;j++)
+            {
+                temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        
+      for(int i=0;i<n;i++)
+        {
+            int l=0,e=n-1;
+            while(l<e){
+                temp=matrix[i][e];
+                matrix[i][e]=matrix[i][l];
+                matrix[i][l]=temp;
+                l++;
+                e--;
+            }
+        }
     }
 };
