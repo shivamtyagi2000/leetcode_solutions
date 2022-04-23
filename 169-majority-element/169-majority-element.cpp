c7a42d31d2e64734aea365b6530eb50fc7f5d9ab
Shivam Tyagi <99984464+shivamtyagi2000@users.noreply.github.com>
2022-04-24 01:57:26 +0530

    Update 169-majority-element.cpp

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,9 +1,34 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        if(nums.size()==1)
-            return nums[0];
+    /*brute force
+        int ans=0,curr=0,temp;
+        for(int i=0;i<nums.size();i++){
+            curr=0;
+            temp=nums[i];
+            for(int j=0;j<nums.size();j++){
+                if(nums[j]==temp)
+                    curr++;
+            }
+            if(curr>(nums.size()/2))
+                return temp;
+        }
+        return -1;*/
+        /* using sorting
         sort(nums.begin(),nums.end());
-        return nums[(nums.size()-1)/2];
+        return nums[nums.size()/2];
+        */
+        
+        // BEST APPAROCH
+        int count=0,ele;
+        for(int i=0;i<nums.size();i++){
+            if(count==0)
+                ele=nums[i];
+            if(nums[i]==ele)
+                count++;
+            else
+                count--;
+        }
+        return ele;
     }
 };
