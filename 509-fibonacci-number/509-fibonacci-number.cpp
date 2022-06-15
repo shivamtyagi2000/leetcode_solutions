6cc452b7bf4bd6dca69395363d34a89230e9c04d
Shivam Tyagi <99984464+shivamtyagi2000@users.noreply.github.com>
2022-06-15 13:11:08 +0530

    Time: 2 ms (63.84%), Space: 5.9 MB (79.98%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,8 +1,32 @@
 class Solution {
 public:
-    int fib(int n) {
-        if(n==0||n==1)
-            return n;
-        return fib(n-1)+fib(n-2);
+    int fib(int N) {
+        //DYNAMIC PROGRAMMING
+   /*    if (N <= 1) {
+            return N;
+        }
+                  
+        int *cache = new int[N + 1];
+        for(int i=0;i<=N;i++)
+            cache[i]=0;
+        cache[1] = 1;
+        for (int i = 2; i <= N; i++) {
+            cache[i] = cache[i - 1] + cache[i - 2];
+        }
+    
+        return cache[N]; */
+        
+        //BEST APPAROCH W/O D.P.
+        int a,b,c;
+        if(N<=1)
+            return N;
+        a=0;
+        b=1;
+        for(int i=2;i<=N;i++){
+            c=a+b;
+            a=b;
+            b=c;
+        }
+        return c;
     }
 };
