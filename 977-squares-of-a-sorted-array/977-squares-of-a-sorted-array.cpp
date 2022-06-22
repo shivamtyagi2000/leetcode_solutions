a99b318d39798d0705704719a1a79ec119438471
Shivam Tyagi <99984464+shivamtyagi2000@users.noreply.github.com>
2022-06-22 15:39:34 +0530

    Time: 61 ms (27.72%), Space: 27 MB (6.18%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int temp1=nums.size()-1;
+        vector<int> ans;
+        int temp=nums.size()-1;
         for(int i=1;i<nums.size();i++){
             if(abs(nums[i-1])<abs(nums[i]))
-               { temp1=i-1;
-                 break;
-               }
+                {temp=i-1;
+                 break;}
         }
-            vector<int> ans;
-            int i=temp1-1,j=temp1+1,temp;
-            temp=nums[temp1]*nums[temp1];
-            ans.push_back(temp);
-            while((i>=0)&&(j<=(nums.size()-1))){
-                if(abs(nums[i])<abs(nums[j]))
-                    {
-                        temp=nums[i]*nums[i];
-                        ans.push_back(temp);
-                        i--;
-                    }
-                   else
-                   {
-                        temp=nums[j]*nums[j];
-                        ans.push_back(temp);
-                        j++;   
-                    }
+        int i=temp-1,j=temp+1;
+        ans.push_back(nums[temp]*nums[temp]);
+        while(i>=0&&j<=nums.size()-1){
+            if(abs(nums[i])<abs(nums[j]))
+            {
+                ans.push_back(nums[i]*nums[i]);
+                i--;
             }
-            while(i>=0){
-                temp=nums[i]*nums[i];
-                        ans.push_back(temp);
-                        i--;
+            else
+            {
+                ans.push_back(nums[j]*nums[j]);
+                j++;
             }
-            while(j<=(nums.size()-1)){
-                 temp=nums[j]*nums[j];
-                        ans.push_back(temp);
-                        j++;
-            }
+        }
+        while(i>=0){
+            ans.push_back(nums[i]*nums[i]);
+            i--;
+        }
+        while(j<=nums.size()-1){
+            ans.push_back(nums[j]*nums[j]);
+            j++;
+        }
         return ans;
     }
 };
