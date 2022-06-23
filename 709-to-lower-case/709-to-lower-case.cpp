eb2bddf7535b27025ed8d642fa83c9f3ea9c3863
Shivam Tyagi <99984464+shivamtyagi2000@users.noreply.github.com>
2022-06-23 14:50:44 +0530

    Time: 3 ms (42.14%), Space: 6 MB (81.59%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.cpp
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     string toLowerCase(string s) {
         for(int i=0;i<s.size();i++)
-        if(s[i]>='A'&&s[i]<='Z')
-        s[i]=s[i]-'A'+'a';
+        {
+            if(s[i]>='A'&&s[i]<='Z')
+                s[i]=s[i]-'A'+'a';
+        }
         return s;
     }
 };
